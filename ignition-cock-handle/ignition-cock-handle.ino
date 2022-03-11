586a480485093fac88b2f0166e9d4c00d925a0bc
joongi <joongy007@naver.com>
2022-03-11 10:24:17 +0900

    update ingition-cock-handle.ino files

--- ignition-cock-handle/ignition-cock-handle.ino
+++ ignition-cock-handle/ignition-cock-handle.ino
@@ -1,6 +1,9 @@
 // 전자저울
 #include <SoftwareSerial.h>
-#define ONE 4
+#define SSR_ONE 5
+#define SSR_TWO 7
+#define SSR_FOUR 9
+#define SSR_EIG 11
 
 //블루투스의 Tx, Rx핀을 2번 3번핀으로 설정
 SoftwareSerial mySerial(2, 3); 
@@ -14,7 +17,31 @@ void setup() {
   
   //블루투스와 아두이노의 통신속도를 9600으로 설정
   mySerial.begin(38400);
+
+  // 4, 5, 6, 7 핀을 input으로 설정
+  for(int i = 5;i<12;i+=2){
+    pinMode(i, INPUT);
+  }
 }
+
+int readSwitchValue(){
+  int one = digitalRead(SSR_ONE);
+  int two = digitalRead(SSR_TWO);
+  int four = digitalRead(SSR_FOUR);
+  int eig = digitalRead(SSR_EIG);
+  Serial.print(one);
+  Serial.print(" ");
+  Serial.print(two);
+  Serial.print(" ");
+  Serial.print(four);
+  Serial.print(" ");
+  Serial.println(eig);
+  Serial.println("----------------");
+  return eig * 8 + four * 4 + two * 2 + one;
+}
+
+int tmp=-1;
+int value=0;
 
 void loop() {
   //블루투스에서 넘어온 데이터가 있다면
@@ -26,6 +53,13 @@ void loop() {
   if (Serial.available()) {   
     //블루투스를 통해 입력된 데이터 전달 
     mySerial.write(Serial.read());  
+  }
+
+  value = readSwitchValue();
+  delay(1000);
+  if(value != tmp){
+    tmp = value;
+//    mySerial.write(tmp);
   }
 
 }
