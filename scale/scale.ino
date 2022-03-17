7491324b07a5b59c5e1598d5165234fae5acff9a
joongi <joongy007@naver.com>
2022-03-17 21:40:26 +0900

    update scale.ino

--- scale/scale.ino
+++ scale/scale.ino
@@ -13,9 +13,9 @@ void setLcdText(String text, int row);
 void setup() {
   // 시리얼 통신의 속도를 9600으로 설정
   //시리얼통신이 연결되지 않았다면 코드 실행을 멈추고 무한 반복
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.println("connect complete");
+//  Serial.begin(9600);
+//  while (!Serial);
+//  Serial.println("connect complete");
   
   //블루투스와 아두이노의 통신속도를 9600으로 설정
   mySerial.begin(38400);
@@ -24,17 +24,20 @@ void setup() {
   lcd.begin();
   setLcdText(" * smart scale *",0);
 
-  scale.set_scale(-226.0);
+  // scale 가중치조절
+  scale.set_scale(226.0);
+  // scale 영점 조절
   scale.tare(10);
 
   pinMode(6, INPUT_PULLUP); // 영점조절 버튼
   pinMode(7, INPUT_PULLUP); // 전송 버튼
 }
 
+// 블루투스로 통신된 데이터
 String inputString = "";
 unsigned long previousMillis = 0;
 char buf[16] = "";
-float value=0.0;
+String value= "";
 
 void loop() {
   unsigned long currentMillis = millis();
@@ -50,33 +53,35 @@ void loop() {
     if(myChar != '\n' && myChar != '\r') inputString += myChar;
 
   }
-  //시리얼모니터에 입력된 데이터가 있다면
-  if (Serial.available()) {   
-    //블루투스를 통해 입력된 데이터 전달 
-    mySerial.write(Serial.read());  
-  }
   
-  //  * 453.592
-  value = scale.get_units();
+  // 무게센서로 부터 값을 가져옴
+  value = String(scale.get_units(), 1);
+  // 1초마다 LCD에 현재 g출력
   if(currentMillis - previousMillis > 999){
-    sprintf(buf, "%14s g", String(value, 1).c_str());
+    sprintf(buf, "%14s g", value.c_str());
     setLcdText(String(buf), 1);
+//    Serial.println(buf);
     previousMillis = currentMillis;
   }
   
 
   // 영점 조절 버튼
   if(digitalRead(6) == 0){
-    setLcdText("zero set starting", 0);
+    setLcdText("zero set start", 0);
+//    Serial.print("영점조절 : ");
     scale.tare(10);
+//    Serial.println(scale.get_offset());
     setLcdText("zero set success ", 0);
   }
 
   // 전송 버튼
   if(digitalRead(7) == 0){
-    setLcdText(" send: " + String(value, 1) +" g", 0);
-    setLcdText(String(buf), 1);;
-    mySerial.write(String(value, 1).c_str());
+    sprintf(buf, "send: %8s g", value.c_str());
+    setLcdText(String(buf), 0);
+    sprintf(buf, "%14s g", value.c_str());
+    setLcdText(String(buf), 1);
+    
+    mySerial.write(value.c_str());
   }
   
   // 블루투스에 수신된 문자열이 있을경우
@@ -84,7 +89,7 @@ void loop() {
     // Serial.println(inputString);
     // 그 문자열이 weight? 이면 현재 무게를 전송(g단위)
     if(inputString == "weight?"){
-      mySerial.write(String(value, 1).c_str());
+      mySerial.write(value.c_str());
 
     // 그 문자열이 offset? 이면 무게센서의 현재 offset 값을 전송
     } else if(inputString == "offset?"){
